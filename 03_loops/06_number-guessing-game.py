ad96964feba4317962fbf3c929655892daa6be9b
Daniel Merkl <daniel.merkl.dm@gmail.com>
2020-10-18 19:40:02 +0200

    added number-guessing-game (only task without solution)

---
+++ 03_loops/06_number-guessing-game.py
@@ -0,0 +1,12 @@
+"""
+Schreibt ein Programm, welches folgendermaßen funktioniert:
+
+Der Computer bestimmt durch Zufall einer Zahl zwischen 1 und 100.
+Der Benutzer soll versuchen, diese Zahl zu erraten.
+Ist die Zahl zu groß, wird "deine gerate Zahl ist zu groß" ausgegeben.
+Ist die Zahl zu klein, wird "deine gerate Zahl ist zu klein" ausgegeben.
+Hat der Benutzer die richtige Zahl erraten, soll "Gewonnen" und die Anzahl der
+Versuche ausgegeben werden.
+"""
+
+# TODO: create solution
