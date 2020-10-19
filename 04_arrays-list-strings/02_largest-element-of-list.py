9c50c928e15df77a4953f6ab3eca69d07636ed67
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-10-19 13:50:35 +0200

    Added missing solutions (sometimes multiple solutions)

--- 04_arrays-list-strings/02_largest-element-of-list.py
+++ 04_arrays-list-strings/02_largest-element-of-list.py
@@ -5,4 +5,18 @@ Beispiel:
 [1, 4, 14, 3, 0] -> 14
 """
 
-# TODO: create solution
+def findBiggestElementInList(listInput):
+    # return max(listInput)
+
+    biggestElement = None
+    for element in listInput:
+        if biggestElement is None or element > biggestElement:
+            biggestElement = element
+
+    return biggestElement
+
+
+listInput = [1, 4, 14, 3, 0]
+biggestElementPresentInList = findBiggestElementInList(listInput)
+
+print(biggestElementPresentInList)
--- 04_arrays-list-strings/03_print-list-reversed.py
+++ 04_arrays-list-strings/03_print-list-reversed.py
@@ -5,4 +5,12 @@ Beispiel:
 [1, 4, 14, 3, 0] -> 0 3 14 4 1
 """
 
-# TODO: create solution
+def reverseList(listInput):
+    for element in listInput[::-1]:
+        print(element)
+
+    # for i in range(len(listInput) - 1, -1, -1):
+    #     print(listInput[i])
+
+
+reverseList([1, 4, 14, 3, 0])
