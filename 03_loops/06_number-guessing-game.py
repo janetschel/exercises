e6cfbd12ef45a122fa8367490a65b64c0993336b
mawaelzl <Dingsbums98>
2020-10-19 15:41:46 +0200

    Deez nuts

--- 03_loops/06_number-guessing-game.py
+++ 03_loops/06_number-guessing-game.py
@@ -9,4 +9,24 @@ Hat der Benutzer die richtige Zahl erraten, soll "Gewonnen" und die Anzahl der
 Versuche ausgegeben werden.
 """
 
-# TODO: create solution
+import random
+
+replay = "y"
+
+print("In wievielen Versuchen kannst du eine Zahl zwischen 1 und 100 erraten?")
+
+while replay == "y":
+    randomNumber = random.randint(1, 100)
+    numberOfGuesses = 0
+
+    while True: #Use do-while if possible
+        guessedNumber = int(input("An welche Zahl denke ich? "))
+        numberOfGuesses += 1
+
+        if guessedNumber == randomNumber:
+            print(f"Korrekt! Du hast {numberOfGuesses} Versuche gebraucht.")
+            break
+        else:
+            print(f"Meine Zahl ist {'größer' if guessedNumber < randomNumber else 'kleiner'} als {guessedNumber}.")
+
+    replay = input("Möchtest du nochmal spielen? (y/n) ")
