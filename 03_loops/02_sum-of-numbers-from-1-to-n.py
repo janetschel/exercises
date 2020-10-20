6abd219f09232194a32151dd0adfe9b4c00ac044
Daniel Merkl <daniel.merkl.dm@gmail.com>
2020-10-20 16:54:54 +0200

    added while-solution

--- 03_loops/02_sum-of-numbers-from-1-to-n.py
+++ 03_loops/02_sum-of-numbers-from-1-to-n.py
@@ -4,13 +4,18 @@ Zahl berechnet, die der Benutzer eingegeben hat.
 Für die Lösung beide Varianten (for- und while-Schleife) implementieren!
 """
 
-userInput = input("Zahl: ")
+userInput: str = input("Zahl: ")
 
-number = int(userInput)
+number: int = int(userInput)
 
 result: int = 0
 
 for n in range(1, number + 1):
     result += n
+
+# i: int = 1
+# while i <= number:
+#     result += i
+#     i += 1
 
 print(f"Die Summe aller Zahlen von 1 bis {userInput} ist: {result}")
