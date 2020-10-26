5ab23a15fd49476ae6901bfbfbb54a714de30723
Daniel Merkl <daniel.merkl.dm@gmail.com>
2020-10-26 18:15:36 +0100

    added exercise number-to-list-of-digits

---
+++ week_02/01_loop_revision/09_number-to-list-of-digits.py
@@ -0,0 +1,25 @@
+"""
+Schreibt eine Funktion, welche eine Nummer in eine Liste bestehend aus den
+einzelnen Ziffern besteht.
+
+Beispiel:
+123 -> [1, 2, 3]
+1337 -> [1, 3, 3, 7]
+
+"""
+from typing import List
+
+
+def numberToDigits(number: int) -> List[int]:
+    result: List[int] = []
+    numberString: str = str(number)
+
+    for character in numberString:
+        digit: int = int(character)
+        result.append(digit)
+
+    return result
+
+
+print(numberToDigits(123)) # [1, 2, 3]
+print(numberToDigits(1337)) # [1, 2, 3]
