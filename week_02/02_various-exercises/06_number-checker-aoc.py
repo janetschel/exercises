fffeeec143cfafcc3ef2ed09e5da760b04f9406d
Manuel Wälzlein <waelzlein.manuel@web.de>
2020-10-26 10:32:29 +0100

    aoc 2019 4.1 und 4.2

---
+++ week_02/02_various-exercises/06_number-checker-aoc.py
@@ -0,0 +1,46 @@
+"""
+You arrive at the Venus fuel depot only to discover it's protected by a password. The Elves had written the password on a sticky note, but someone threw it out.
+
+However, they do remember a few key facts about the password:
+
+It is a six-digit number.
+The value is within the range given in your puzzle input.
+Two adjacent digits are the same (like 22 in 122345).
+Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
+Other than the range rule, the following are true:
+
+111111 meets these criteria (double 11, never decreases).
+223450 does not meet these criteria (decreasing pair of digits 50).
+123789 does not meet these criteria (no double).
+How many different passwords within the range given in your puzzle input meet these criteria?
+
+Input: 356261, 846303
+"""
+
+
+def check_adjacent(passcode_str):
+    for i in range(len(passcode_str) - 1):
+        if passcode_str[i] == passcode_str[i + 1]:
+            return True
+    return False
+
+
+def check_ascending_or_equal(passcode_str):
+    for i in range(len(passcode_str) - 1):
+        if passcode_str[i] > passcode_str[i + 1]:
+            return False
+    return True
+
+
+def check_passcode(passcode):
+    passcode_str = str(passcode)
+    return check_adjacent(passcode_str) and check_ascending_or_equal(passcode_str)
+
+
+number_of_passcodes = 0
+
+for j in range(356261, 846304):
+    if check_passcode(j):
+        number_of_passcodes += 1
+
+print(number_of_passcodes)
---
+++ week_02/02_various-exercises/07_number-checker-aoc2.py
@@ -0,0 +1,41 @@
+"""
+An Elf just remembered one more important detail: the two adjacent matching digits are not part of a larger group of matching digits.
+
+Given this additional criterion, but still ignoring the range rule, the following are now true:
+
+112233 meets these criteria because the digits never decrease and all repeated digits are exactly two digits long.
+123444 no longer meets the criteria (the repeated 44 is part of a larger group of 444).
+111122 meets the criteria (even though 1 is repeated more than twice, it still contains a double 22).
+How many different passwords within the range given in your puzzle input meet all of the criteria?
+"""
+
+def check_valid_number_of_equals(str_to_count):
+    for x in range(10):
+        number_of_occurences = 0
+        for letter in str_to_count:
+            if letter == str(x):
+                number_of_occurences += 1
+        if number_of_occurences == 2:
+            return True
+    return False
+
+
+def check_ascending_or_equal(passcode_str):
+    for i in range(len(passcode_str) - 1):
+        if passcode_str[i] > passcode_str[i + 1]:
+            return False
+    return True
+
+
+def check_passcode(passcode):
+    passcode_str = str(passcode)
+    return check_ascending_or_equal(passcode_str) and check_valid_number_of_equals(passcode_str)
+
+
+number_of_passcodes = 0
+
+for j in range(356261, 846304):
+    if check_passcode(j):
+        number_of_passcodes += 1
+
+print(number_of_passcodes)
